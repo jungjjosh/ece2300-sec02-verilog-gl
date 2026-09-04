6ae0fd67db1484b547bf754d026b75726edbb47a
jj826 <jj826@cornell.edu>
2026-09-04 16:29:54 -0400

    finished implementation

--- PairTripleDetector_GL-test.v
+++ PairTripleDetector_GL-test.v
@@ -71,7 +71,7 @@ module Top();
 
     //     in0 in1 in2 out
     check( 0,  0,  0,  0 );
-
+    
     t.test_case_end();
   endtask
 
@@ -82,9 +82,15 @@ module Top();
   task test_case_2_exhaustive();
     t.test_case_begin( "test_case_2_exhaustive" );
 
-    //''' ACTIVITY '''''''''''''''''''''''''''''''''''''''''''''''''''''''
-    // Add checks for exhaustive testing (check all possible inputs)
-    //''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+    check( 0, 0, 0, 0 );
+    check( 0, 0, 1, 0 );
+    check( 0, 1, 0, 0 );
+    check( 0, 1, 1, 1 );
+
+    check( 1, 0, 0, 0 );
+    check( 1, 0, 1, 1 );
+    check( 1, 1, 0, 1 );
+    check( 1, 1, 1, 1 );
 
     t.test_case_end();
   endtask
--- PairTripleDetector_GL.v
+++ PairTripleDetector_GL.v
@@ -27,6 +27,15 @@ module PairTripleDetector_GL
   `ECE2300_UNUSED( in2 );
   `ECE2300_FLOATING( out );
 
+  wire w;
+  wire x;
+  wire y;
+
+  or(w,in0,in1);
+  and(x,in0,in1);
+  and(y,w,in2);
+  or(out,y,x);
+
 endmodule
 
 `endif /* PAIR_TRIPLE_DETECTOR_GL_V */
